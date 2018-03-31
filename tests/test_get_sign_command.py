1f7e2cabc3b968add0b62dfb7312bc2bef6f0895
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-03-31 20:12:28 +0300

    Skel

---
+++ tests/test_get_sign_command.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+import windows_signtool
+
+
+class Test_get_sign_command(unittest.TestCase):
+
+    def test_fake(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ windows_signtool.py
@@ -0,0 +1,10 @@
+# Module for find signtool.exe and generate sign command
+# Dmitriy Vetutnev, Odant, 2018
+
+
+def get_signtool_path(arch=None):
+    pass
+
+
+def get_sign_command(file, digest_algorithm=None, timestamp_server=None, signtool_path=None, arch=None):
+    pass

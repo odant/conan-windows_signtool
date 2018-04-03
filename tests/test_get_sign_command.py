07c433885d66c9f37c77fce3c13658a0a93e23ae
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-04-03 16:27:28 +0300

    get_sign_command arch

--- tests/test_get_sign_command.py
+++ tests/test_get_sign_command.py
@@ -1,13 +1,42 @@
 import unittest
+import mock as mock
 
 
 import windows_signtool
 
 
-class Test_get_sign_command(unittest.TestCase):
+class Test_get_sign_command_arch(unittest.TestCase):
 
-    def test_fake(self):
-        pass
+    @mock.patch("windows_signtool.get_signtool_path")
+    def test_arch_None(self, mock_get_signtool_path):
+        mock_get_signtool_path.return_value = "C:/blablabla/bin/x64/signtool.exe"
+        calls = [
+        ]
+        #
+        windows_signtool.get_sign_command("D:/build/binary.exe")
+        #
+        mock_get_signtool_path.assert_called_once_with(None)
 
+    @mock.patch("windows_signtool.get_signtool_path")
+    def test_arch_x86(self, mock_get_signtool_path):
+        mock_get_signtool_path.return_value = "C:/blablabla/bin/x86/signtool.exe"
+        calls = [
+        ]
+        #
+        windows_signtool.get_sign_command("D:/build/binary.exe", arch="x86")
+        #
+        mock_get_signtool_path.assert_called_once_with("x86")
+
+    @mock.patch("windows_signtool.get_signtool_path")
+    def test_arch_x86_64(self, mock_get_signtool_path):
+        mock_get_signtool_path.return_value = "C:/blablabla/bin/x64/signtool.exe"
+        calls = [
+        ]
+        #
+        windows_signtool.get_sign_command("D:/build/binary.exe", arch="x86_64")
+        #
+        mock_get_signtool_path.assert_called_once_with("x86_64")
+
+    
 if __name__ == "__main__":
     unittest.main()
--- windows_signtool.py
+++ windows_signtool.py
@@ -6,5 +6,11 @@ def get_signtool_path(arch=None):
     pass
 
 
-def get_sign_command(file, digest_algorithm=None, timestamp_server=None, signtool_path=None, arch=None):
-    pass
+def get_sign_command(
+        file,
+        digest_algorithm="sha1",
+        timestamp_server="http://timestamp.verisign.com/scripts/timestamp.dll",
+        signtool_path=None,
+        arch=None):
+    #
+    get_signtool_path(arch)

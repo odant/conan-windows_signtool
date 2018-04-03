72387cbd949c1fdf90184277a769e2bc592aadc5
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-04-03 16:32:01 +0300

    Test get_sign_command_arch clean

--- tests/test_get_sign_command.py
+++ tests/test_get_sign_command.py
@@ -10,8 +10,6 @@ class Test_get_sign_command_arch(unittest.TestCase):
     @mock.patch("windows_signtool.get_signtool_path")
     def test_arch_None(self, mock_get_signtool_path):
         mock_get_signtool_path.return_value = "C:/blablabla/bin/x64/signtool.exe"
-        calls = [
-        ]
         #
         windows_signtool.get_sign_command("D:/build/binary.exe")
         #
@@ -20,8 +18,6 @@ class Test_get_sign_command_arch(unittest.TestCase):
     @mock.patch("windows_signtool.get_signtool_path")
     def test_arch_x86(self, mock_get_signtool_path):
         mock_get_signtool_path.return_value = "C:/blablabla/bin/x86/signtool.exe"
-        calls = [
-        ]
         #
         windows_signtool.get_sign_command("D:/build/binary.exe", arch="x86")
         #
@@ -30,8 +26,6 @@ class Test_get_sign_command_arch(unittest.TestCase):
     @mock.patch("windows_signtool.get_signtool_path")
     def test_arch_x86_64(self, mock_get_signtool_path):
         mock_get_signtool_path.return_value = "C:/blablabla/bin/x64/signtool.exe"
-        calls = [
-        ]
         #
         windows_signtool.get_sign_command("D:/build/binary.exe", arch="x86_64")
         #

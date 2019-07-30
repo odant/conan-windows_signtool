f4c41bc4ce0af07941fa2b90f03a16909eecc1fb
d0nc1h0t <>
2019-07-30 17:07:05 +0300

    Version 1.1

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile
 
 class Conan_windows_signtool(ConanFile):
     name = "windows_signtool"
-    version = "1.0"
+    version = "1.1"
     license = "MIT"
     url = "https://github.com/odant/conan-windows_signtool"
     description = "Python module for find MS signtool.exe and generate sign command"
